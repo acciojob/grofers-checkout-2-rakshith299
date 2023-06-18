f3c0c24295b25c3e125b844f27dceaabbc9cde99
rakshith299 <133012343+rakshith299@users.noreply.github.com>
2023-06-18 22:47:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-//your code here
+const table = document.getElementById("table");
+const arr = (document.querySelectorAll("td[data-ns-test]"));
+
+let numArr = [];
+
+for(let i = 0; i < arr.length; i++){
+    numArr.push(Number(arr[i].innerHTML.trim()));
+}
+
+let sum = 0;
+for(let i = 0; i < numArr.length; i++){
+    sum = sum + numArr[i];
+}
+
+//data-ns-test="grandTotal"
+let newRow = document.createElement("tr");
+newRow.setAttribute("data-ns-test", "grandTotal");
+newRow.innerText = sum;
+
+table.appendChild(newRow);
+
+console.log(table);
+
